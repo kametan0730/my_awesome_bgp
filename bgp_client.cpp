bb10cbef4dc14b382e941a95dfd78570db30a34a
kametan0730 <kametan0730@gmail.com>
2021-08-23 19:53:15 +0900

    修正

--- bgp_client.cpp
+++ bgp_client.cpp
@@ -69,23 +69,27 @@ bool bgp_client_loop(int sock){
             total_path_attribute_length = ntohs(total_path_attribute_length);
             log(log_level::DEBUG, "%d", total_path_attribute_length);
             break;
-            case NOTIFICATION:
-            {
-                log(log_level::INFO, "\e[34mNotification received");
-                auto* bgpntp = reinterpret_cast<bgp_notification*>(buff);
-                log(log_level::INFO, "  Error: %d", bgpntp->error);
-                log(log_level::INFO, "  Sub: %d", bgpntp->error_sub);
+        case NOTIFICATION:
+        {
+            log(log_level::INFO, "\e[34mNotification received");
+            auto* bgpntp = reinterpret_cast<bgp_notification*>(buff);
+            log(log_level::INFO, "Error: %d", bgpntp->error);
+            log(log_level::INFO, "Sub: %d", bgpntp->error_sub);
+        }
+        case KEEPALIVE:
+            log(log_level::INFO, "\e[35mKeepalive Received");
+            bgp_header header;
+            memset(header.maker, 0xff, 16);
+            header.length = htons(19);
+            header.type = KEEPALIVE;
+            if (send(sock, &header, len, 0) <= 0) {
+                log(log_level::ERROR, "Failed to send packet");
+                return false;
             }
-            case KEEPALIVE:
-                log(log_level::INFO, "\e[35mKeepalive Received");
-                if (send(sock, buff, len, 0) <= 0) {
-                    log(log_level::ERROR, "Failed to send packet");
-                    return false;
-                }
-                break;
-                default:
-                    log(log_level::WARNING, "\e[7mUnknown type received %d", bgphp->type);
-                    break;
+            break;
+        default:
+            log(log_level::WARNING, "\e[7mUnknown type received %d", bgphp->type);
+            break;
     }
     return true;
 }
